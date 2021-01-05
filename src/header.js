e34d4d4007e5205289814f44298b432e6476c018
sushil70 <sushil29121997@gmail.com>
2021-01-05 11:15:55 +0530

    init2

--- src/header.js
+++ src/header.js
@@ -1,1 +1,12 @@
 import "./App";
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default Header;
